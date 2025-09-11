ced226b344f550cb55ab4b3a4f8ef820d0f898f5
Alelu27 <20221423@aloe.ulima.edu.pe>
2025-09-10 21:33:00 -0500

    Laboratorio_04

---
+++ Laboratorio_04/src/main/java/com/mycompany/laboratorio_04/PortafolioSystem.java
@@ -0,0 +1,7 @@
+package com.mycompany.laboratorio_04;
+
+public class PortafolioSystem {
+    public void getFinancialData(){
+        System.out.println("Se ha obtenido la data financiera!!!");
+    }
+}
---
+++ Laboratorio_04/src/main/java/com/mycompany/laboratorio_04/ReportSystem.java
@@ -0,0 +1,7 @@
+package com.mycompany.laboratorio_04;
+
+public class ReportSystem {
+    public void generateReport(){
+        System.out.println("Se ha generado el reporte!!!");
+    }
+}
